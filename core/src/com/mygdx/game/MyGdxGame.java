707d3d2975392b3d74dd74fe87c2542367ea24c7
ZefCorsen <julien.gambier.morel@gmail.com>
2015-01-21 16:52:39 +0100

    Testing command recognition

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,23 +1,17 @@
 package com.mygdx.game;
-
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.TimeUtils;
-import com.badlogic.gdx.math.MathUtils;
+
 
 public class MyGdxGame extends Game {
 	SpriteBatch batch;
-	Texture img;
     private BitmapFont font;
-    int i=0;
+
 	
 	@Override
 	public void create () {
--- core/src/com/mygdx/screens/GameScreen.java
+++ core/src/com/mygdx/screens/GameScreen.java
@@ -4,13 +4,19 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.MyGdxGame;
 
 /**
  * Created by Whole lotta love on 21/01/2015.
  */
 public class GameScreen implements Screen {
 
+    MyGdxGame game;
 
+    public GameScreen(){
+
+        game= new MyGdxGame();
+    }
     @Override
     public void show() {
 
