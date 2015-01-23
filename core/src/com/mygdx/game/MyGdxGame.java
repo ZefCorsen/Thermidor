fc8096d75287c8f694b31c18b369c8daeb4ba09a
ZefCorsen <julien.gambier.morel@gmail.com>
2015-01-23 11:15:37 +0100

    Clean Branch

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -8,15 +8,7 @@ import com.mygdx.world.World;
 public class MyGdxGame extends Game {
     private World world;
 
-<<<<<<< HEAD
-=======
-        batch = new SpriteBatch();
-        font = new BitmapFont();
-        font.setColor(Color.RED);
-        font.setScale(12);
-        
-	}
->>>>>>> origin/Sprint-1
+
 
     @Override
     public void create() {
@@ -24,24 +16,5 @@ public class MyGdxGame extends Game {
         setScreen(new MainMenu(this));
     }
 
-<<<<<<< HEAD
-=======
-    @Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-        if (Gdx.input.isTouched()) {
-            i++;
-            font.draw(batch, "hello"+i, 200, 200);
-        }
-        batch.end();
-    }
 
-
-    public void refresh() {
-        font.draw(batch, "GUY", 50, 100);
-
-    }
->>>>>>> origin/Sprint-1
 }
--- core/src/com/mygdx/screens/GameScreen.java
+++ core/src/com/mygdx/screens/GameScreen.java
@@ -1,6 +1,6 @@
 package com.mygdx.screens;
 
-<<<<<<< HEAD
+
 /**
  * Created by Jerem on 21/01/2015.
  */
@@ -128,45 +128,6 @@ public class GameScreen implements Screen, InputProcessor {
     @Override
     public boolean scrolled(int amount) {
         return false;
-=======
-import com.badlogic.gdx.Screen;
-/**
- * Created by Whole lotta love on 21/01/2015.
- */
-public class GameScreen implements Screen {
-    @Override
-    public void show() {
 
-    }
-
-    @Override
-    public void render(float delta) {
-
-    }
-
-    @Override
-    public void resize(int width, int height) {
-
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-
-    }
-
-    @Override
-    public void dispose() {
-
->>>>>>> origin/Sprint-1
     }
 }
